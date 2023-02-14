a2d007d11e31d596a45afc9e0484225adec9c532
Husnain Raza <Husnain Raza@DESKTOP-604O976>
2023-02-14 04:33:58 -0800

    Add project files.

---
+++ Tcp_Clients_chat/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace Tcp_Clients_chat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("This is client");
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ip = host.AddressList[0];
+                IPEndPoint remote = new IPEndPoint(ip, 12000);
+
+                Socket sender = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                sender.Connect(remote);
+
+
+                byte[] buffer = new byte[1024];
+                while (true)
+                {
+
+                    string data = Console.ReadLine();
+
+                    buffer = Encoding.UTF8.GetBytes(data); 
+
+                    sender.Send(buffer);
+                    byte[] buffer2 = new byte[1024];
+
+                    sender.Receive(buffer2);
+
+                    data = Encoding.UTF8.GetString(buffer2);
+
+                    Console.WriteLine("server : " + data.Replace("\0", "").TrimEnd().TrimStart());
+                    if (data.Replace("\0", "").TrimEnd().TrimStart().Equals("Bye", StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+                }
+                sender.Shutdown(SocketShutdown.Both);
+
+                sender.Close();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+        }
+    }
+}
---
+++ server/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+namespace server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Console Chat");
+
+            try
+            {
+                Console.WriteLine("This is Server! ");
+
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                /// IPAddress ip = IPAddress.Parse("");
+                IPAddress ip = host.AddressList[0];
+
+                IPEndPoint ipe = new IPEndPoint(ip, 12000);
+
+                Socket listener = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                listener.Bind(ipe);
+
+                listener.Listen(10);
+
+                Socket handler = listener.Accept();
+
+                string data = "";
+
+                while (true)
+                {
+                    byte[] buffer = new byte[1024];
+                    handler.Receive(buffer);
+
+                    data = Encoding.ASCII.GetString(buffer);
+
+                    Console.WriteLine("Client: " + data.Replace("\0", "").TrimEnd().TrimStart());
+
+                    byte[] buffer2 = new byte[1024];
+                    data = Console.ReadLine();
+
+                    buffer2 = Encoding.ASCII.GetBytes(data);
+                    handler.Send(buffer2);
+                    if (data.Replace("\0", "").TrimEnd().TrimStart().Equals("Bye", StringComparison.OrdinalIgnoreCase))
+                    {
+                        break;
+                    }
+
+
+                }
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
